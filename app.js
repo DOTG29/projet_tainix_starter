bc78b91d50977832c4392121ecf01cdfa46b811d
DOTG29 <dgoris@edenschool.fr>
2023-03-16 16:13:30 +0100

    exersice tainix

---
+++ app.js
@@ -0,0 +1,46 @@
+/* variable et calcul ⭐*/
+
+// const side = 29;
+
+// let perimetre = side*4;
+// let aire = side*side;
+// let result = perimetre + aire;
+
+// console.log(result);
+
+/* variable et concatenation ⭐*/
+
+// const nom = 'Beatrice';
+// const room = 'home office';
+// let result = nom + ' is in the ' + room + '.';
+
+// console.log(result);
+
+/* boucle for ⭐*/
+
+// const stops = 88;
+// let result = 0;
+
+// for (let i = 0; i < stops; i++) {
+//     result += i;
+// }
+
+// console.log(result);
+
+/* boucle foreach ⭐*/
+
+// const values = [3, 4, 2, 5, 2, 9, 10, 2, 9, 2, 8, 3, 6, 8, 8, 7, 2, 7, 5, 3, 3, 8, 4, 6, 1];
+
+// let result = 0;
+
+// values.forEach(function(mot){
+//     if (mot >= 5) {
+//         result += mot 
+//     }
+// })
+// console.log(result);
+
+
+
+
+
